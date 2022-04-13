d6e0ec1d6bb2334184b65c084bea291a4a012390
Holder19 <Manhunt19@inbox.ru>
2022-04-13 22:49:23 +0300

    Делает закрытие меню на оверлей и исправляет ошибки переполнения

--- source/js/main.js
+++ source/js/main.js
@@ -10,41 +10,69 @@ const inputCheckbox = bookingForm.querySelector('#checkbox');
 const logo = document.querySelector('#logo');
 const overlay = document.querySelector('.overlay');
 const navList = document.querySelector('#nav-list');
+const blueLogo = document.querySelector('.nav__logo-wrapper_blue');
 
 const navMenuToggler = () => {
   nav.classList.remove('nav--nojs');
   nav.classList.add('nav--closed');
 
   navToggle.addEventListener('click', () => {
-    if (nav.classList.contains('nav--closed')) {
+    if (nav.classList.contains('nav--closed') && nav.offsetWidth < 767) {
       nav.classList.remove('nav--closed');
       nav.classList.add('nav--opened');
       logo.style.visibility = 'hidden';
       overlay.classList.add('overlay--visible');
       document.body.style.overflowY = 'hidden';
+      navList.style.overflowY = 'scroll';
       navList.focus();
+      blueLogo.classList.add('nav__logo-wrapper_blue--shown');
     } else {
       nav.classList.remove('nav--opened');
       logo.style.visibility = 'initial';
       nav.classList.add('nav--closed');
       overlay.classList.remove('overlay--visible');
       document.body.style.overflowY = 'inherit';
+      blueLogo.classList.remove('nav__logo-wrapper_blue--shown');
+      navList.style.overflowY = 'initial';
     }
   });
 };
 
-const handlerOnNavList = (evt) => {
-  if (evt.target.closest('li')) {
+const handlerOnNavListScroll = () => {
+  let navScrolled = navList.scrollTop;
+  if (navScrolled > 10) {
+    blueLogo.classList.remove('nav__logo-wrapper_blue--shown');
+  } else {
+    blueLogo.classList.add('nav__logo-wrapper_blue--shown');
+  }
+};
+
+const handlerOnNavListClick = (evt) => {
+  if (evt.target.closest('li') && nav.offsetWidth < 767) {
     document.body.style.overflowY = 'inherit';
     overlay.classList.remove('overlay--visible');
     nav.classList.remove('nav--opened');
     nav.classList.add('nav--closed');
     logo.style.visibility = 'initial';
+    blueLogo.classList.remove('nav__logo-wrapper_blue--shown');
+    navList.style.overflowY = 'initial';
   } else {
-    document.body.style.overflowY = 'hidden';
     nav.classList.add('nav--opened');
-    logo.style.visibility = 'hidden';
     nav.classList.remove('nav--closed');
+  }
+};
+
+const handlerOnOverlayClick = (evt) => {
+  if (evt.target === overlay && nav.offsetWidth < 767) {
+    overlay.classList.remove('overlay--visible');
+    nav.classList.remove('nav--opened');
+    nav.classList.add('nav--closed');
+    logo.style.visibility = 'initial';
+    document.body.style.overflowY = 'inherit';
+    blueLogo.classList.remove('nav__logo-wrapper_blue--shown');
+    navList.style.overflowY = 'initial';
+  } else {
+    overlay.classList.add('overlay--visible');
   }
 };
 
@@ -102,4 +130,6 @@ const addFormListeners = () => {
 
 addFormListeners();
 navMenuToggler();
-navList.addEventListener('click', handlerOnNavList);
+navList.addEventListener('click', handlerOnNavListClick);
+overlay.addEventListener('click', handlerOnOverlayClick);
+navList.addEventListener('scroll', handlerOnNavListScroll);
